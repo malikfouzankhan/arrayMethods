2cfaf54df5a7f34705f86fd5d1be405f00798c64
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:48:16 +0000

    added indexOf().js

---
+++ indexOf().js
@@ -0,0 +1,6 @@
+// indexOf() method
+// returns the index of the given argument
+// returns -1 if the value does not exist in the array
+
+let arr = [101,201,301,401,501];
+console.log(arr.indexOf(400));
