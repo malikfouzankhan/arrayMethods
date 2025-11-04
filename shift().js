86cbd726f2b7f348f7e47530709668fc8ef67a69
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 06:34:57 +0000

    added shift().js

---
+++ shift().js
@@ -0,0 +1,7 @@
+// shift() method
+// removes and returns the first element of the array
+// NOTE : shift() is an in-place method which means it will change the original array
+
+let arr = [0,1,2,3,4,5,6,7,8,9];
+console.log(arr.shift());
+console.log(arr);
