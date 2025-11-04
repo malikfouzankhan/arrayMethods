37b400c9794b74b1d58b1e72e08874183b0996a6
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 08:26:49 +0000

    added find().js

---
+++ find().js
@@ -0,0 +1,6 @@
+// find() method
+// returns the first element that satisfies the given condition
+// returns undefined if no element found
+
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(arr.find((x) => x > 7));
