0cbe42c43b415816907cb2c9984d12daad8cbbc0
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:45:42 +0000

    added fill().js

---
+++ fill().js
@@ -0,0 +1,7 @@
+// fill() method
+// replaces all the elements with the given argument
+// NOTE : fill() is an in-place method which means it will change the original array
+
+let arr = [1,2,3,4,5,6,7,8,9];
+console.log(arr.fill("Hey"));
+// console.log(arr);
