7ccb95ed6f61a856f55c1c38fb0ddefb5595a836
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 06:31:52 +0000

    added unshift().js

--- unshift().js
+++ unshift().js
@@ -1,6 +1,8 @@
 // unshift() method
+// inserts an element before the first element of the array
+// NOTE : unshift() is an in-place method which means it will change the original array
 
 let arr = [1,2,3,4,5,6,7,8,9];
+console.log(arr.unshift(0)); // This line will insert the element and return the new length of the array
 // arr.unshift(0);
-console.log(arr.unshift("Hello"));
 // console.log(arr);
