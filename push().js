fc2d8e798031ac55cc4455ea4a7255d2338631e2
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:56:16 +0000

    added push().js

---
+++ push().js
@@ -0,0 +1,8 @@
+// push() method
+// inserts an element after the last element of the array
+// NOTE : push() is an in-place method which means it will change the original array
+
+let arr = [1,2,3,4];
+// console.log(arr.push(5));
+arr.push(5);
+console.log(arr);
