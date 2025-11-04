f1ea4f598b986c14e928fe2b9a91e8f62e4b2e5e
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:58:30 +0000

    added pop().js

---
+++ pop().js
@@ -0,0 +1,8 @@
+// pop() method
+// removes and returns the last element of the array
+// NOTE : pop() is an in-place method which means it will change the original array
+
+let arr = [1,2,3,4,5,6,7,8,9,0];
+// console.log(arr.pop());
+arr.pop();
+console.log(arr);
