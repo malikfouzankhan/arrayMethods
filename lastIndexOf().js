8e9b345dc8d777b9a6e47a09c2b4921b80472c1e
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:50:25 +0000

    added lastIndexOf().js

---
+++ lastIndexOf().js
@@ -0,0 +1,6 @@
+// lastIndexOf() method
+// returns the index of the given value but starts searching from the end of the array
+// returns -1 if the value does not exist
+
+let arr = [100,200,300,100,500];
+console.log(arr.lastIndexOf(101));
