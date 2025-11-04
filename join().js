8279eb2205748bf33f8a89dec85d7c53c8cddd79
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 07:36:57 +0000

    added join().js

--- join().js
+++ join().js
@@ -1,5 +1,6 @@
 // join() method
 // returns as output which concatenates all elements using the delimiter given in function
+// It is NOT an in-place method
 
 let arr = [1,2,3,4,5,6,7,8,9];
 console.log(arr.join("#"));
---
+++ reverse().js
@@ -0,0 +1,5 @@
+// reverse() method
+// 
+let arr = [99,88,77,66,55,44,33,22,11];
+arr.reverse();
+console.log(arr);
