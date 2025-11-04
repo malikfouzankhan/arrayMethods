55e3c6ae1755a0994adfd705320b080a51d14761
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:42:21 +0000

    added concat().js

---
+++ concat().js
@@ -0,0 +1,11 @@
+// concat() method
+// merges the two arrays into one in the order [arr1, arr2]
+// it is different from arr1 + arr2
+let arr1 = [1,2,3,4,5];
+let arr2 = [6,7,8,9,0];
+console.log("This is concatenation:",arr1.concat(arr2));
+console.log("\nThis is adding two arrays using '+' operator:\n\n",arr1 + arr2);
+
+let str1 = ["I","am"];
+let str2 = ["Malik","Fouzan"];
+console.log("\n\n",str1.concat(str2));
