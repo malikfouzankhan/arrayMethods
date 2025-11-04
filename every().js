784d471b51fba1e92b1ce1782d0da81b361d2046
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 08:24:25 +0000

    added every().js

---
+++ every().js
@@ -0,0 +1,6 @@
+// every() method
+// checks for a condition given in the arrow function
+// returns true if all the elements pass otherwise false
+
+let arr = [10,20,30,40,50];
+console.log(arr.every((x) => x > 10));
