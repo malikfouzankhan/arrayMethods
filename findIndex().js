79433fb0c1f97ad561bb9259e1f9c052f77d6a60
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 08:28:56 +0000

    added findIndex().js

---
+++ findIndex().js
@@ -0,0 +1,6 @@
+// findIndex() method
+// returns the index of the first value that satisfies the given condition
+// returns -1 if no element found
+
+let arr = [10,15,20,25,30,35,40];
+console.log(arr.findIndex((x) => x > 30));
