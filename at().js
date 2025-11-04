1c89b13ee3c849aedd1293c861b8ccdc61354298
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-04 05:37:01 +0000

    added at().js

---
+++ at().js
@@ -0,0 +1,6 @@
+// at() method
+// this method returns the value at a given index and returns undefined if the index is out of bounds
+// NOTE : it also works with negative indexing
+
+let arr = [1,2,3,"hello","fouzan",true];
+console.log(arr.at(-3));
