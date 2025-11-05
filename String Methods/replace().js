f1deccde0f1138f221760ae547510e5836008a45
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 12:11:56 +0000

    added replace().js

---
+++ String Methods/replace().js
@@ -0,0 +1,6 @@
+// replace() method
+// replaces the first argument with the second argument
+// it is NOT an in-place method
+
+let str = "Hello World!!";
+console.log(str.replace("World", "CFI"));
