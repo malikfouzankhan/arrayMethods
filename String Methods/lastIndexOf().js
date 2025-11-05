f514bff5fae5f43b2eccaf7adcc72a71d90e65ce
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 10:10:46 +0000

    added lastIndexOf().js

---
+++ String Methods/lastIndexOf().js
@@ -0,0 +1,6 @@
+// lastIndexOf() method
+// returns the index of the first character of the argument but starts chceking from the end of the string
+// similar to indexOf() method
+
+let str = "classmate Pulse";
+console.log(str.lastIndexOf('se'));
