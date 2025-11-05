7a41028eb50f54682ec373648953272b944bb827
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 12:28:08 +0000

    added trim().js

---
+++ String Methods/trim().js
@@ -0,0 +1,6 @@
+// trim() method
+// returns a string by removing all white spaces in the beginning and end of the string
+// it is NOT an in-place method
+
+let str = "   Code For India Foundation   ";
+console.log(str.trim());
