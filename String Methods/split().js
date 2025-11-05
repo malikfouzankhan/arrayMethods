b0782a98c9a092c335dff53710393b4f91ade2e5
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 12:17:57 +0000

    added split().js

---
+++ String Methods/split().js
@@ -0,0 +1,6 @@
+// split() method
+// returns an array split by the given argument(it could be any datatype)
+// it is NOT an in-place method
+
+let str = "10520530540560";
+console.log(str.split(5));
