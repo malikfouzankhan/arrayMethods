e86aa7d31566699719c5289729807d033b6228db
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 12:08:49 +0000

    added repeat().js

---
+++ String Methods/repeat().js
@@ -0,0 +1,6 @@
+// repeat() method
+// repeats the given string the number of times given in the argument
+// if any datatype other than number is given as argument then it is tried to convert into number using implicit conversion.
+
+let str = "Hello World!!";
+console.log(str.repeat(5));
