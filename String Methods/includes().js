329ce9c3361d1cba453a58d33ca20d030d25e1bf
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 09:53:38 +0000

    added includes().js

---
+++ String Methods/includes().js
@@ -0,0 +1,6 @@
+// includes() method
+// returns a boolean value if the given argument exists in the string
+// the argument can even contain white spaces
+
+let str = "Code For India Foundation";
+console.log(str.includes(" Fo"));
