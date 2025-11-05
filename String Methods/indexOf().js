09b93a0ed3bfeb7a9fe5902e87a789e5b6c3ccd9
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 09:58:17 +0000

    added indexOf().js

---
+++ String Methods/indexOf().js
@@ -0,0 +1,6 @@
+// indexOf() method
+// returns the index of the given argument 
+// will return the index of the first character if argument contains a string/substring
+
+let str = "EdVenture Park";
+console.log(str.indexOf('Venture'));
