afda6726fa8767988e76b5fda2e3b88f0cb48ca8
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 09:55:57 +0000

    added length.js

---
+++ String Methods/length.js
@@ -0,0 +1,6 @@
+// length property
+// returns a number representing the length of the array
+// number of characters in the string including white spaces
+
+let str = "Full Stack Development";
+console.log(str.length);
