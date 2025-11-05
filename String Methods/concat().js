f6dd75731e0471e55fb17a8bdaecd8b0775c9d42
malikfouzankhan <malikfouzan05@gmail.com>
2025-11-05 09:48:29 +0000

    added concat().js

---
+++ String Methods/concat().js
@@ -0,0 +1,10 @@
+// concat() method
+// returns a string with both the string's content added
+// string concat() method is equivalent to '+' operator
+
+let str1 = "Malik";
+let str2 = "Fouzan";
+
+console.log(str1.concat(str2));
+// it is equivalent to:
+console.log(str1 + str2);
